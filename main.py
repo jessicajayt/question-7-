9c1c3556bfa72e4b3889dfdb1b130d5c1572c23d
jessicajayt <65548888+jessicajayt@users.noreply.github.com>
2020-05-19 17:44:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for line in f:
   radius.append(float(line.split('\t')[0]))
   velocity.append(float(line.split('\t')[1]))
   radius1.append((line.split('\t')[2]))
-  velocity1.append(float(line.split('\t')[4]))
+  velocity1.append(float(line.split('\t')[3]))
   mass.append(float(line.split('\t')[4]))
   predictedVelocity.append((float(G))*(float(line.split('\t')[4]))/(float(line.split('\t')[0]))**(0.5))
 
