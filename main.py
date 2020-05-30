beea2df4862005cb9a36b66af14f27d44ec61e85
jessicajayt <65548888+jessicajayt@users.noreply.github.com>
2020-05-31 00:35:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ mass = []
 predictedVelocity = []
 G = 4.30*(10**-6)
 
+
 for line in f:
   radius.append(float(line.split('\t')[0]))
   velocity.append(float(line.split('\t')[1]))
   mass.append(float(line.split('\t')[4]))
-  predictedVelocity.append((float(G))*(float(line.split('\t')[4]))/(float(line.split('\t')[0]))**(0.5))
+  predictedVelocity.append(((float(G))*(float(line.split('\t')[4]))/(float(line.split('\t')[0])))**(1/2))
 
 x = np.array(radius)
 y = np.array(velocity)
 z = np.array(predictedVelocity)
 
-plt.plot(x,y)
-plt.plot(x,z)
+
+plt.plot(x,y) #This is the velocity against the radius
+plt.plot(x,z) #This is the predicted velocity against the radius
 plt.ylabel('Velocity(km/h)')
 plt.xlabel('Radius (kpc)')
 plt.show()
